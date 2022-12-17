444d41e76d9fd4081469649975fcc0c52a291249
Warlock <internalmike@gmail.com>
2022-12-17 17:45:45 +0300

    start

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,105 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![feature(never_type)]
+
+use nom::{self, IResult};
+use nom::error::ParseError;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum NomErr<E, F> {
+    Error(E),
+    Failure(F),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<E, F> NomErr<E, F> {
+    pub fn map_err<X>(self, f: impl FnOnce(E) -> X) -> NomErr<X, F> {
+        match self {
+            NomErr::Error(e) => NomErr::Error(f(e)),
+            NomErr::Failure(e) => NomErr::Failure(e),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn map_fail<X>(self, f: impl FnOnce(F) -> X) -> NomErr<E, X> {
+        match self {
+            NomErr::Error(e) => NomErr::Error(e),
+            NomErr::Failure(e) => NomErr::Failure(f(e)),
+        }
+    }
+}
+
+impl<F> NomErr<!, F> {
+    pub fn any_err<E>(self) -> NomErr<E, F> {
+        self.map_err(|x| x)
+    }
+}
+
+impl<E> NomErr<E, !> {
+    pub fn any_fail<F>(self) -> NomErr<E, F> {
+        self.map_fail(|x| x)
+    }
+}
+
+impl<E> NomErr<E, E> {
+    pub fn fail(self) -> NomErr<!, E> {
+        match self {
+            NomErr::Error(e) => NomErr::Failure(e),
+            NomErr::Failure(e) => NomErr::Failure(e),
+        }
+    }
+}
+
+impl<I, E, F> ParseError<I> for NomErr<E, F> {
+    fn from_error_kind(_input: I, _kind: nom::error::ErrorKind) -> Self { panic!() }
+
+    fn append(_input: I, _kind: nom::error::ErrorKind, _other: Self) -> Self { panic!() }
+
+    fn or(self, _other: Self) -> Self { panic!() }
+
+    fn from_char(_input: I, _: char) -> Self { panic!() }
+}
+
+pub type NomRes<I, O, E, F> = IResult<I, O, NomErr<E, F>>;
+
+pub fn parser_from_result<I, O, E, F>(r: Result<(I, O), NomErr<E, F>>) -> NomRes<I, O, E, F> {
+    match r {
+        Ok((i, o)) => Ok((i, o)),
+        Err(NomErr::Error(e)) => Err(nom::Err::Error(NomErr::Error(e))),
+        Err(NomErr::Failure(f)) => Err(nom::Err::Failure(NomErr::Failure(f))),
+    }
+}
+
+pub fn result_from_parser<I, O, E, F>(r: NomRes<I, O, E, F>) -> Result<(I, O), NomErr<E, F>> {
+    match r {
+        Ok((i, o)) => Ok((i, o)),
+        Err(nom::Err::Error(NomErr::Error(e))) => Err(NomErr::Error(e)),
+        Err(nom::Err::Failure(NomErr::Failure(e))) => Err(NomErr::Failure(e)),
+        _ => panic!()
+    }
+}
+
+pub fn map_err<I: Clone, O, E, F, X>(
+    mut parser: impl FnMut(I) -> NomRes<I, O, E, F>,
+    mut f: impl FnMut(E, I) -> X
+) -> impl FnMut(I) -> NomRes<I, O, X, F> {
+    move |input: I| {
+        parser_from_result(result_from_parser(parser(input.clone())).map_err(|e| e.map_err(|e| f(e, input))))
+    }
+}
+
+pub fn any_err<I: Clone, O, F, X>(
+    mut parser: impl FnMut(I) -> NomRes<I, O, !, F>,
+) -> impl FnMut(I) -> NomRes<I, O, X, F> {
+    move |input: I| {
+        parser_from_result(result_from_parser(parser(input.clone())).map_err(|e| e.any_err()))
+    }
+}
+
+pub fn alt_2<I: Clone, O, E, F, X1>(
+    mut a: impl FnMut(I) -> NomRes<I, O, X1, F>,
+    mut b: impl FnMut(I) -> NomRes<I, O, E, F>,
+) -> impl FnMut(I) -> NomRes<I, O, E, F> {
+    move |input: I| {
+        match a(input.clone()) {
+            Ok(r) => Ok(r),
+            Err(_) => b(input)
+        }
     }
 }
